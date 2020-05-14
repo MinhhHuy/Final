51d8d8cbc4556ced5686c80b05e66128af662223
MinhhHuy <62527656+MinhhHuy@users.noreply.github.com>
2020-05-15 02:12:13 +0700

    first
    first

---
+++ Final/fTableManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final
+{
+    public partial class fTableManager : Form
+    {
+        public fTableManager()
+        {
+            InitializeComponent();
+        }
+
+        private void fTableManager_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nmDisCount_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheckOut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDiscount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fAccountProfile f = new fAccountProfile();
+            f.ShowDialog();
+        }
+
+        private void nmFoodCount_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void adminToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fAdmin f = new fAdmin();
+            f.ShowDialog();
+        }
+    }
+}
